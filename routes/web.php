41c0f807749b5493f8cb5d9e5850fab741503bab
abdelghaniAit <aaittamghart8@gmail.com>
2024-03-06 12:08:30 +0100

    Add Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResereveController;
+use App\Http\Controllers\UserController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +21,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard')->middleware('role:admin,organizer');
+    Route::post('/reserve/{event}', [ResereveController::class, "reserve"])->name('reserve');
+
+    Route::get('reservations', [ResereveController::class, 'index'])->name('reservations.index');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('events', EventController::class);
+    Route::get('users', [UserController::class, 'index'])->name("users.index");
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
