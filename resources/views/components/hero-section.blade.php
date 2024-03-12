71c530eb9b0dc9ded7522c0ee5bd5fce93cb019f
abdelghaniAit <aaittamghart8@gmail.com>
2024-03-12 23:12:44 +0000

    Edit hero section

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -8,8 +8,43 @@
                 </h1>
                 <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
                     plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot
-                    chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                </div>
+                    chicken authentic tumeric truffaut hexagon try-hard chambray.
+                </p>
+                <form action="#" class="seva-form formkit-form w-full flex-grow"
+                    method="post" data-sv-form="4692392" data-uid="344e3b5c48" data-format="inline" data-version="5"
+                    min-width="400 500 600">
+                    <div data-style="clean" class="flex items-end mb-3 w-full">
+                        <div data-element="fields" data-stacked="false"
+                            class="flex flex-col gap-3 md:flex-row md:gap-0 items-center w-full mb-2 seva-fields formkit-fields">
+                            <div class="relative md:w-2/3 w-full mr-3 formkit-field">
+                                <label for="member_email"
+                                    class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email
+                                    address</label>
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                                    <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                        <path
+                                            d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z">
+                                        </path>
+                                        <path
+                                            d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z">
+                                        </path>
+                                    </svg>
+                                </div>
+                                <input id="member_email"
+                                    class="formkit-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    name="email_address" aria-label="Email Address" placeholder="Your email address..."
+                                    required="" type="email">
+                            </div>
+                            <button data-element="submit" class="formkit-submit w-full md:w-auto">
+                                <span
+                                    class="px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg cursor-pointer hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Become a Organizer
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                 <img class="object-cover object-center rounded" alt="hero" src="{{ asset('hero_photo.jpg') }}">
             </div>
